d89be753d8d63041c58abe1c8182c85467cfc492
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-27 15:44:58 +0200

    struttura header

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <a href="#">
+                        <img src="../assets/img/dc-logo.png" alt="">
+                    </a>
+                </div>
+                <div class="col-8 ">
+                    <ul class="d-flex justify-content-between justify-content-center align-items-center">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .row{
+        margin-top: 50px;    
+        padding: 20px;
+
+        img{
+            width: 75px;
+        }
+
+        ul{
+            height: 100%;
+            margin: 0;
+        }
+    }
+</style>
