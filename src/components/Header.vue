055bfc96282a6c2afe4989e2c9ef6c07702a166f
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-29 17:19:53 +0200

    dinamicità al nav e classe active aggiunta

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,18 +8,11 @@
                     </a>
                 </div>
                 <div class="col-8 ">
-                    <ul class="d-flex justify-content-between justify-content-center align-items-center">
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                    </ul>
+                    <nav>
+                        <ul class="d-flex justify-content-between justify-content-center align-items-center">
+                            <li v-for="(element, index) in navLInk" :key="index" :class="{active : element.active}"><a :href='element.link' :class="{active : element.active}" >{{element.name}}</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
@@ -30,22 +23,101 @@
 
 <script>
 export default {
+    name: 'Header',
 
+    data: function(){
+        return{
+            navLInk: [
+                {
+                    name: 'charachters',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'comics',
+                    link: '#',
+                    active: true
+                },
+                {
+                    name: 'movies',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'tv',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'games',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'collectibles',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'videos',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'fans',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'news',
+                    link: '#',
+                    active: false
+                },
+                {
+                    name: 'shop',
+                    link: '#',
+                    active: false
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import './style/variables.scss';
     .row{
-        margin-top: 50px;    
-        padding: 20px;
+      padding-top:50px;
 
         img{
             width: 75px;
         }
+        nav{
+            line-height: 75px;
 
-        ul{
-            height: 100%;
-            margin: 0;
+            ul{
+                height: 100%;
+                margin: 0;
+                display: flex;
+                justify-content: space-evenly;
+                li{
+                    list-style-type: none;
+                    height: 100%;
+                    &.active{
+                        color: $bluCeleste;
+                        border-bottom: 3px solid $bluCeleste;
+                    }
+                    a{
+                        color: black;
+                        text-transform: uppercase;
+                        font-weight: 700;
+                        font-size: 13px;
+
+                    }
+                }
+            }
         }
+        
+        
     }
 </style>
