4d37ddde4bedb42a73856b9b9cd4b122400890da
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-29 16:42:43 +0200

    struttura footer completa

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="row">
-        <div class="container">
-            <div class="col-6 colonna" id="left_column">
-                <div class="col-4 " v-for="(link, index) in links" :key='index'>
-                    <h2>{{link.name}}</h2>
-                    <li v-for="(element, indice) in links[index].linkList" :key="indice" ><a href="#" >{{element}}</a></li>
+    <footer>
+        <div class="row" id="row_bg">
+            <div class="container" >
+                <div class="col-6 colonna" id="left_column">
+                    <div class="col-3 piccola_colonna" v-for="(link, index) in links" :key='index'>
+                        <h2>{{link.name}}</h2>
+                        <li v-for="(element, indice) in links[index].linkList" :key="indice" ><a href="#" >{{element}}</a></li>
+                    </div>
+                </div>
+                <div class="col-6 colonna" id="dc_bg"></div>
+            </div>
+        </div>
+        <div class="row" id="scnd_row">
+            <div class="container">
+                <div class="col-6">
+                    <button>sing-up now!</button>
+                </div>
+                <div class="col-6">
+                    
+                    <ul>
+                        <li><h2>follow us</h2></li>
+                        <li><a href=""><img src="../assets/img/footer-facebook.png" alt=""></a></li>
+                        <li><a href=""><img src="../assets/img/footer-twitter.png" alt=""></a></li>
+                        <li><a href=""><img src="../assets/img/footer-youtube.png" alt=""></a></li>
+                        <li><a href=""><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
+                        <li><a href=""><img src="../assets/img/footer-periscope.png" alt=""></a></li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-6 colonna" id="dc_bg"></div>
         </div>
-    </div>
+        
+    </footer>
 </template>
 
 <script>
@@ -76,28 +97,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.row{
-    height: 400px;
+@import './style/variables.scss';
+#row_bg{
     background-image: url('../assets/img/footer-bg.jpg');
     background-repeat: no-repeat;
     background-position:center ;
     background-size: cover;
-
+    height: 500px;
+}
+.row{
     .container{
         display: flex;
+
+        .piccola_colonna{
+            padding-top: 50px
+        }
         li{
             list-style-type: none;
         }
         a{
             color: grey;
-            font-size: 13px
+            font-size: 12px
         }
         h2{
             color: white;
             text-transform: uppercase;
             font-weight: 700;
-            font-size: 18px;
+            font-size: 17px;
         }
         #left_column{
             display: flex;
@@ -110,6 +136,34 @@ export default {
         #dc_bg{
             background-image: url('../assets/img/dc-logo-bg.png');
             background-position: center ;
+        }
+    }
+}
+
+#scnd_row{
+    height: 100px;
+    background-color: rgb(56, 56, 56);
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        button{
+            text-transform: uppercase;
+            color: white;
+            height: 50px;
+            width: 150px;
+            border: 2px solid $bluCeleste;
+            background-color:rgb(56, 56, 56);
+        }
+        h2{
+            font-size: 1.3em;
+            color: $bluCeleste;
+            margin: 0;
+            height: 100%;
+        }
+        ul{
+            display: flex;
+            justify-content: space-evenly;
         }
     }
 }
